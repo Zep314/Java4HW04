e75d819d56583c188a747ac210e9387e8655e2b2
Dmitry Mordanov <mordanov314@gmail.com>
2023-03-14 08:51:57 +0300

    initial commit

---
+++ src/controller/Controller.java
@@ -0,0 +1,13 @@
+package controller;
+
+//import model.DoublyList;
+//import view.View;
+
+public class Controller {  // Контроллер
+
+    public Controller() {
+    }
+    public void run(){
+        System.out.println("Hi!");
+    }
+}
